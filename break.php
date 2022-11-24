b0834dab0f7e76070661559747a1dcfc337d355b
GunAce1 <108649246+GunAce1@users.noreply.github.com>
2022-11-24 10:51:31 +0545

    Add files via upload

---
+++ break.php
@@ -0,0 +1,15 @@
+<?php
+$marks=56;
+
+
+if($marks >="80"){
+    echo "dude got distiction and obtained  $marks marks";
+}
+elseif($marks >="70"){
+    echo "dude you got first division obtained $marks marks";
+}
+else{
+    echo "dude you just bearly passed $marks marks";
+
+}
+?>
