2daed369b02ac5ccd95b8276ac84d498311783cf
GunAce1 <108649246+GunAce1@users.noreply.github.com>
2022-11-24 10:51:56 +0545

    Add files via upload

---
+++ gun.php
@@ -0,0 +1,14 @@
+<?php
+$x = "my name is sonam don." ;
+
+
+
+$i = 10;
+$y = 5;
+
+$d = "The sum of $i and $y =" ;
+$s = "The multiple of $i and $y =" ;
+$c = "The difference between $i and $y =";
+
+echo $x . "<br>", " " , $d , " " , $i + $y . "<br>", " ", $s , " ", $i*$y. "<br>", $c , " ", $i-$y . "<br>";
+?>
